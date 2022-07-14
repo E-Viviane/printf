545a2cad3ebafb9f3bf07f3b6ed7e06e91a48a5a
E-Viviane <104867990+E-Viviane@users.noreply.github.com>
2022-07-14 23:35:59 +0100

    Update put_string.c

--- put_string.c
+++ put_string.c
@@ -1,51 +1,26 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- * * put_string - specificer s
- *
- * * @v: list of arguments
- *
- * * Return: total characters printed
- *
- * */
-
+* put_string - specificer s
+* @v: list of arguments
+* Return: total characters printed
+*/
 int put_string(va_list v)
-
 {
+	int i;
+	char *s;
 
-		int i;
+	s = va_arg(v, char*);
 
-			char *s;
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
 
-
-				s = va_arg(v, char*);
-
-
-
-					if (s == NULL)
-
-							{
-
-										s = "(null)";
-
-											}
-
-
-
-						for (i = 0; s[i] != '\0'; i++)
-
-								{
-
-											_putchar(s[i]);
-
-												}
-
-
-
-							return (i);
-
+	return (i);
 }
